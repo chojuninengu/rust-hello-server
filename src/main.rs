3db640ffd1bf38cd2483d2ca3aeb762f3926bd2a
chojuninengu <chojuninengu@gmail.com>
2025-03-10 19:59:03 +0100

    working version

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
-use hyper::Server; // Import Server from hyper
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    // Define the router with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    // Define the address to bind to
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await.unwrap();
 
-    // Start the server
     println!("Listening on http://{}", addr);
-    Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
